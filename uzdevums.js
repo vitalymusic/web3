5d361c747781f67d3887dc5af38a3184d2bf55d1
vitalymusic <vitalijs.korabelnikovs@gmail.com>
2026-09-15 14:50:33 +0300

    Kalkulators

---
+++ uzdevums.js
@@ -0,0 +1,5 @@
+// Ķermeņa masas indekss = svars / augums
+let svars = prompt("Tavs svars");
+let augums = prompt("Tavs augums (metros)");
+document.body.innerHTML = "Tavs ķermeņa indekss ir: "+ svars / (augums*augums)
+
