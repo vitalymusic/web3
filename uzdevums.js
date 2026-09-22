789309d530f5de809784bfa5c634635b28fb49f7
vitalymusic <vitalijs.korabelnikovs@gmail.com>
2026-09-22 12:48:29 +0300

    2.stunda

--- uzdevums.js
+++ uzdevums.js
@@ -1,5 +1,12 @@
-// Ķermeņa masas indekss = svars / augums
+// Ķermeņa masas indekss = svars / augums(kvadrātā)
 let svars = prompt("Tavs svars");
-let augums = prompt("Tavs augums (metros)");
-document.body.innerHTML = "Tavs ķermeņa indekss ir: "+ svars / (augums*augums)
+let augums = prompt("Tavs augums (centimetros)");
+document.body.innerHTML = "Tavs ķermeņa indekss ir: "+ (svars / (augums/100)**2).toFixed(2)
+
+// Izmainīt tā, lai augumu var rasktīt centimetros
+
+
+
+
+
 
