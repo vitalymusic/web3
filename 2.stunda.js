4a1f27a4603899f0b605b4403dc4e1a8c05a9fd1
vitalymusic <vitalijs.korabelnikovs@gmail.com>
2026-09-22 14:42:29 +0300

    2.stunda

--- 2.stunda.js
+++ 2.stunda.js
@@ -29,12 +29,44 @@ let krasa = {
 document.body.style.color = `rgb(${krasa.red},${krasa.green},${krasa.blue})`;
 }
 
-setInterval(krasuMaina,1000)
+// setInterval(krasuMaina,1000)
+
+
+// darbības ar teksta virknēm
+
+let teksts1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non consequuntur ut esse porro ipsam! Obcaecati eaque incidunt rem eos. Rem fugit distinctio ex quae harum, itaque quisquam maxime vel dolor.";
 
 
 
 
+let iznakums = teksts1.length;
+    iznakums = teksts1.toUpperCase();
+    iznakums = teksts1.toLowerCase();
+    // iznakums = teksts1.split("");
 
+    // iznakums = teksts1.slice(0,10);
 
+    iznakums = teksts1.replaceAll("Lorem","<u>Mākslas un mēdiju tehnikums</u>")
+    iznakums = iznakums.repeat(3)
+document.body.innerHTML+=iznakums
 
 console.log(rezultats)
+
+
+
+    // Uzrakstīt programmu, kas prasa cik bildes izvadīt, un pēc ievades atkārtot <img src=""> kodu
+    // Bildes adresei izmantot picsum.photos
+    // rezultātu izvadīt ar document.body.innerHTML = '';
+    // *izmantojot skaitļu ģenerāciju random() pie katras bildes adrese pievienot ID atribūtu ar randoma skaitli no 0-255, ieskatīties picsum.photos
+
+    let bildesHTML = `<img src="https://picsum.photos/500">`;
+
+
+    let uzraksts = "<h1>Bilžu galerija</h1>";
+
+
+
+
+
+    
+
